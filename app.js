95af1bf2e4227ed16ebb930ae7a46c2cd24ddadb
tod619 <trevorodwyer619@hotmail.com>
2020-03-23 21:08:28 +0000

    Test search btn to be sure that it works on click

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const random = document.getElementById("random");
 const mealsEl = document.getElementById("meals");
 const resultHeading = document.getElementById("result-heading");
 const single_mealEl = document.getElementById("single-meal");
+
+// Functions
+// SearchMeal function which will search for a meal and fetch from api
+function searchMeal(e) {
+  // Prevent default behaviour
+  e.preventDefault();
+
+  console.log("search");
+}
+
+// Event Listners
+submit.addEventListener("submit", searchMeal);
