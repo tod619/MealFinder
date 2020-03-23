1aeeb34a01a29b80fcdc6888c2201a573db8c751
tod619 <trevorodwyer619@hotmail.com>
2020-03-23 17:21:04 +0000

    Add variables to app.js file

---
+++ app.js
@@ -0,0 +1,6 @@
+const search = document.getElementById("search");
+const submit = document.getElementById("submit");
+const random = document.getElementById("random");
+const mealsEl = document.getElementById("meals");
+const resultHeading = document.getElementById("result-heading");
+const single_mealEl = document.getElementById("single-meal");
