e14fd4761a3df6a36ec3b63694918661014b1829
tod619 <trevorodwyer619@hotmail.com>
2020-03-24 13:46:18 +0000

    Add meal search functionality in app.js file

--- app.js
+++ app.js
@@ -11,7 +11,42 @@ function searchMeal(e) {
   // Prevent default behaviour
   e.preventDefault();
 
-  console.log("search");
+  // Clear Single Meal
+  single_mealEl.innerHTML = "";
+
+  // Get Search Term
+  const term = search.value;
+
+  // Check for empty
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no results for ${term}. Please try again!</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              meal => `
+            <div class = "meal">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+              <div class="meal-info" data-mealID="meal.idMeal">
+                <h3>${meal.strMeal}<h3>
+
+              </div>
+            </div>
+          `
+            )
+            .join("");
+        }
+      });
+    // Clear Search Text
+    search.value = "";
+  } else {
+    alert("Please Enter A Search Value!");
+  }
 }
 
 // Event Listners
